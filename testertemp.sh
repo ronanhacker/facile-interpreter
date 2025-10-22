a2f1fe600423f333e596fba826a9259e49d65e1c
Jane P <badidea2004@gmail.com>
2025-10-22 10:48:45 -0500

    fixed printuninit

--- testertemp.sh
+++ testertemp.sh
@@ -3,8 +3,20 @@ do
     javac "$filename"
 done
 
+errorsstill=0
 for file in DemoPrograms/*
 do
     echo "$file"
     java facile.java "$file"
+    errorsinfile=$(java facile.java $file | grep -c 'ERROR.')
+    let "errorsstill = $errorsstill + $errorsinfile"
+    echo ""
 done
+
+if [ $errorsstill -gt 1 ]; then
+    echo "There are $errorsstill tests that still return 'ERROR.'"
+elif [ $errorsstill -eq 1 ]; then
+    echo "There is 1 test that still returns 'Error.'"
+else
+    echo "No tests still return 'Error!'"
+fi
