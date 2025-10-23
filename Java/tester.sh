c66ec4da02f496479bad4b2bf819464acfdc7ae6
Ronan Hacker <hackerr@carleton.edu>
2025-10-23 00:15:24 -0500

    made folder for Java interpreter

---
+++ Java/tester.sh
@@ -0,0 +1,26 @@
+for filename in ./*.java
+do
+    javac "$filename"
+done
+
+errorsstill=0
+numtests=$(ls tests | wc -l)
+let "numtests = $numtests + 1"
+
+for i in $(seq 1 $numtests);
+do
+    echo "=== ${bold}Test $i${normal} ==="
+    echo ""
+    java facile.java "Tests/Test$i.txt" 
+    errorsinfile=$(java facile.java "Tests/Test$i.txt" | grep -c 'ERROR.')
+    let "errorsstill = $errorsstill + $errorsinfile"
+    echo ""
+done
+
+if [ $errorsstill -gt 1 ]; then
+    echo "There are $errorsstill tests that still return 'ERROR.'"
+elif [ $errorsstill -eq 1 ]; then
+    echo "There is 1 test that still returns 'Error.'"
+else
+    echo "No tests still return 'Error!'"
+fi
